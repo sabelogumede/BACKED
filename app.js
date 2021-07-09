a33df1757f97d780ddce912da3a422c49f8ce0b1
sabelogumede <iamsabelogumede@gmail.com>
2021-07-09 13:24:52 +0200

    added post request with middleware express.js to hadle our json data

--- app.js
+++ app.js
@@ -5,11 +5,24 @@ require("dotenv/config");
 
 const api = process.env.API_URL;
 
-app.get("/", (req, res) => {
-  res.send("hello API!");
+// Middleware
+app.use(express.json());
+
+app.get(`${api}/products`, (req, res) => {
+  const product = {
+    id: 1,
+    name: "hair dresser",
+    image: "some_url",
+  };
+  res.send(product);
+});
+
+app.post(`${api}/products`, (req, res) => {
+  const newProduct = req.body;
+  console.log(newProduct);
+  res.send(newProduct);
 });
 
 app.listen(3000, () => {
-  console.log(api);
   console.log("server running http://localhost:3000");
 });
